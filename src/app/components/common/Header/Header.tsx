16c569693b622becd62411916618f9e64c697d0e
armd482 <armd482@naver.com>
2024-04-16 01:20:35 +0900

    UI: notification

--- src/app/components/common/Header/Header.tsx
+++ src/app/components/common/Header/Header.tsx
@@ -5,9 +5,14 @@ import Link from 'next/link';
 import { useEffect, useState, useRef } from 'react';
 import styles from './Header.module.scss';
 
-interface HeaderProps {
-  memberType: string;
-}
+type NotificationDataType = {
+  id: string;
+  name: string;
+  startsAt: string;
+  createdAt: string;
+  result: string;
+  read: boolean;
+};
 
 type ButtonType = {
   name: string;
@@ -50,22 +55,38 @@ const BUTTON_LIST: ButtonListType = {
   },
 };
 
-export default function Header({ memberType }: HeaderProps) {
+interface HeaderProps {
+  memberType: string;
+  notificationListData: NotificationDataType[];
+}
+export default function Header({ memberType, notificationListData }: HeaderProps) {
   const buttonType = BUTTON_LIST[memberType];
   const [isOpen, setIsOpen] = useState(false);
-  const notifiacationRef = useRef<HTMLDivElement>(null);
+  const [notificationData, setNotificationData] = useState(notificationListData);
+  const notificationNumber = notificationData.length;
+  const notificationRef = useRef<HTMLDivElement>(null);
   const handleClickNotificationButton = () => {
-    setIsOpen((prev) => !prev);
+    setIsOpen((previousStatus) => !previousStatus);
+  };
+  const handleClickCloseButton = () => {
+    setIsOpen(false);
   };
   useEffect(() => {
     const handleClickOutside = (event: Event) => {
-      if (isOpen && notifiacationRef.current && !notifiacationRef.current?.contains(event.target as Node)) {
+      if (isOpen && notificationRef.current && !notificationRef.current?.contains(event.target as Node)) {
         setIsOpen(false);
       }
     };
     document.addEventListener('click', handleClickOutside);
 
     return () => document.removeEventListener('click', handleClickOutside);
+  }, [isOpen]);
+
+  /* 창 닫았을 때, 읽었던 데이터 삭제 */
+  useEffect(() => {
+    if (!isOpen) {
+      setNotificationData((previousData) => previousData.filter((notification) => !notification.read && notification));
+    }
   }, [isOpen]);
 
   return (
@@ -94,7 +115,7 @@ export default function Header({ memberType }: HeaderProps) {
           ),
         )}
         {buttonType.notification && (
-          <div className={styles.notifiacation}>
+          <div className={styles.notification}>
             <Image
               src="/notification_active.svg"
               alt="notification"
@@ -105,8 +126,43 @@ export default function Header({ memberType }: HeaderProps) {
               role="presentation"
             />
             {isOpen && (
-              <div className={styles.notificationWindow} ref={notifiacationRef}>
-                test
+              <div className={styles.notificationWindow} ref={notificationRef}>
+                <div className={styles.notificationTitleWrapper}>
+                  <div className={styles.notificationTitle}>{`알림 ${notificationNumber}개`}</div>
+                  <Image
+                    src="/close.svg"
+                    alt="close"
+                    width={24}
+                    height={24}
+                    className={styles.notificationClose}
+                    onClick={handleClickCloseButton}
+                  />
+                </div>
+                <div className={styles.notificationList}>
+                  {notificationData.map((notification) => (
+                    <div key={notification.id} className={styles.notificationWrapper}>
+                      <div
+                        className={
+                          notification.result === 'accepted' ? styles.notificationBlueFin : styles.notificationRedFin
+                        }
+                      />
+                      <div className={styles.notificationText}>
+                        HS 과일주스(2023-01-14 15:00~18:00) 공고 지원이&nbsp;
+                        <span
+                          className={
+                            notification.result === 'accepted'
+                              ? styles.notificationBlueStatus
+                              : styles.notificationRedStatus
+                          }
+                        >
+                          {notification.result === 'accepted' ? '승인' : '거절'}
+                        </span>
+                        되었어요.
+                      </div>
+                      <div className={styles.notificationDate}>7분전</div>
+                    </div>
+                  ))}
+                </div>
               </div>
             )}
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
 import Header from './components/common/Header/Header';
 
 export default function Home() {
+  const data = [
+    {
+      id: '1',
+      name: 'test1',
+      startsAt: '2024-04-16T15:00:00.000Z',
+      createdAt: '2024-04-15T15:06:15.633Z',
+      result: 'accepted',
+      read: false,
+    },
+    {
+      id: '2',
+      name: 'test3',
+      startsAt: '2024-04-16T15:00:00.000Z',
+      createdAt: '2024-04-15T15:06:15.633Z',
+      result: 'rejected',
+      read: false,
+    },
+    {
+      id: '3',
+      name: 'test3',
+      startsAt: '2024-04-16T15:00:00.000Z',
+      createdAt: '2024-04-15T15:06:15.633Z',
+      result: 'rejected',
+      read: false,
+    },
+    {
+      id: '4',
+      name: 'test3',
+      startsAt: '2024-04-16T15:00:00.000Z',
+      createdAt: '2024-04-15T15:06:15.633Z',
+      result: 'rejected',
+      read: false,
+    },
+  ];
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <Header memberType="worker" />
+      <Header memberType="worker" notificationListData={data} />
     </div>
   );
 }
