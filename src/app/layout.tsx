214be1d0c3c7effd13cd7fb8b2618825c31d2182
zoonyoung <9851248@gmail.com>
2024-04-16 13:27:58 +0900

    Feat: 리셋, 미디어, 믹스인, 변수 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import '@/styles/_reset.scss';
+import '@/styles/_common.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
