d60a7deecfbba1955cda0feeb6dc9793265f373d
Taejin Kim <rlaxowls1316@naver.com>
2024-04-18 00:16:12 +0900

    Feat: 신청내역 UI추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,9 @@ import styles from './Profile.module.scss';
 
 export default function Profile() {
   const isProfileExist = false;
+  /*
+    받아온 데이터에 name, phone, address, bio가 없으면 isProfileExist를 false로
+  */
   return (
     <div className={styles.container}>
       <div className={styles.header}>..</div>
@@ -10,7 +13,7 @@ export default function Profile() {
         <section className={styles.profile}>
           <div className={styles.wrapper}>
             {isProfileExist ? (
-              <>asd</>
+              <>adfjo</>
             ) : (
               <>
                 <h2 className={styles.sectionTitle}>내 프로필</h2>
@@ -21,6 +24,12 @@ export default function Profile() {
                 />
               </>
             )}
+          </div>
+        </section>
+        <section className={styles.applyList}>
+          <div className={styles.wrapper}>
+            <h2 className={styles.sectionTitle}>신청 내역</h2>
+            <Banner description="아직 신청 내역이 없어요." buttonContent="공고 보러가기" linkPath="/" />
           </div>
         </section>
       </main>
