b508474805dedc9879cbaf501ca23520ff5db3b0
Taejin Kim <rlaxowls1316@naver.com>
2024-04-17 23:34:31 +0900

    Feat: banner 컴포넌트 Link 추가

--- src/app/profile/Banner.tsx
+++ src/app/profile/Banner.tsx
@@ -1,18 +1,22 @@
 import Button from '@/components/common/Button';
+import Link from 'next/link';
 import styles from './Banner.module.scss';
 
 type BannerProps = {
   description: string;
   buttonContent: string;
+  linkPath: string;
 };
 
-export default function Banner({ description, buttonContent }: BannerProps) {
+export default function Banner({ description, buttonContent, linkPath }: BannerProps) {
   return (
     <div className={styles.banner}>
       <p className={styles.bannerDescription}>{description}</p>
-      <Button color="orange" size="medium" style={{ width: '37rem', margin: '0 auto' }}>
-        {buttonContent}
-      </Button>
+      <Link style={{ textDecoration: 'none' }} href={linkPath}>
+        <Button color="orange" size="medium" style={{ maxWidth: '37rem', margin: '0 auto' }}>
+          {buttonContent}
+        </Button>
+      </Link>
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,17 +2,25 @@ import Banner from './Banner';
 import styles from './Profile.module.scss';
 
 export default function Profile() {
+  const isProfileExist = false;
   return (
     <div className={styles.container}>
       <div className={styles.header}>..</div>
       <main className={styles.main}>
         <section className={styles.profile}>
           <div className={styles.wrapper}>
-            <h2 className={styles.sectionTitle}>내 프로필</h2>
-            <Banner
-              description="내 프로필을 등록하고 원하는 가게에 지원해 보세요."
-              buttonContent="내 프로필 등록하기"
-            />
+            {isProfileExist ? (
+              <>asd</>
+            ) : (
+              <>
+                <h2 className={styles.sectionTitle}>내 프로필</h2>
+                <Banner
+                  description="내 프로필을 등록하고 원하는 가게에 지원해 보세요."
+                  buttonContent="내 프로필 등록하기"
+                  linkPath="/"
+                />
+              </>
+            )}
           </div>
         </section>
       </main>
