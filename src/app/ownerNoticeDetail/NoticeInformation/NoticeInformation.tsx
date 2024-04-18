710f4175879e923ff3fb9ff75cc34a84d2fb9224
armd482 <armd482@naver.com>
2024-04-18 12:39:25 +0900

    Refactor: 공고 내용 컴포넌트 텍스트 스타일링 구조 변경

--- src/app/ownerNoticeDetail/NoticeInformation/NoticeInformation.tsx
+++ src/app/ownerNoticeDetail/NoticeInformation/NoticeInformation.tsx
@@ -17,7 +17,7 @@ export default function NoticeInformation({ noticeData, storeData }: NoticeInfor
     <div className={styles.wrapper}>
       <div className={styles.storeWrapper}>
         <div className={styles.storeTitleWrapper}>
-          <div className={styles.storeCategory}>{storeData.category}</div>
+          <div className={`${styles.storeCategory} ${styles.textMediumMedium}`}>{storeData.category}</div>
           <div className={styles.storeName}>{storeData.name}</div>
         </div>
         <div className={styles.noticeWrapper}>
@@ -32,7 +32,7 @@ export default function NoticeInformation({ noticeData, storeData }: NoticeInfor
           <div className={styles.noticeFlexWrapper}>
             <div className={styles.noticeContentWrapper}>
               <div className={styles.noticeTitleWrapper}>
-                <div className={styles.noticeTitleText}>시급</div>
+                <div className={`${styles.noticeTitleText} ${styles.textMediumMedium}`}>시급</div>
                 <div className={styles.noticeTitleContentWrapper}>
                   <p>{`${noticeData.hourlyPay.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원`}</p>
                   {viewLabel && (
@@ -43,15 +43,15 @@ export default function NoticeInformation({ noticeData, storeData }: NoticeInfor
                   )}
                 </div>
               </div>
-              <div className={styles.noticeLabelWrapper}>
+              <div className={`${styles.noticeLabelWrapper} ${styles.textMediumLarge}`}>
                 <Image src={CLOCK} alt="시간" width={20} height={20} className={styles.notionLabelImage} />
                 <span>{`${getTimeDifference(noticeData.startsAt, noticeData.workhour)} (${noticeData.workhour}시간)`}</span>
               </div>
-              <div className={styles.noticeLabelWrapper}>
+              <div className={`${styles.noticeLabelWrapper} ${styles.textMediumLarge}`}>
                 <Image src={GPS} alt="위치" width={20} height={20} className={styles.notionLabelImage} />
                 <span>{storeData.address1}</span>
               </div>
-              <pre className={styles.storeDescription}>{storeData.description}</pre>
+              <pre className={`${styles.storeDescription} ${styles.textMediumLarge}`}>{storeData.description}</pre>
             </div>
             <Button color="white" size="large">
               공고 편집
@@ -59,8 +59,8 @@ export default function NoticeInformation({ noticeData, storeData }: NoticeInfor
           </div>
         </div>
         <div className={styles.noticeDescription}>
-          <div className={styles.descriptionTitlte}>공고 설명</div>
-          <pre className={styles.descriptionContent}>{noticeData.description}</pre>
+          <div className={`${styles.descriptionTitlte} ${styles.textMediumMedium}`}>공고 설명</div>
+          <pre className={`${styles.descriptionContent} ${styles.textMediumLarge}`}>{noticeData.description}</pre>
         </div>
       </div>
     </div>
