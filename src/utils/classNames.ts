5dc862bead1a145596caf5bd7678a8c261b2144c
zoonyoung <9851248@gmail.com>
2024-04-18 13:43:52 +0900

    Refactor: cn함수 매개변수 타입 추가

--- src/utils/classNames.ts
+++ src/utils/classNames.ts
@@ -1,3 +1,3 @@
-const cn = (...classNameList) => classNameList.filter((className) => className).join(' ');
+const cn = (...classNameList: string[]) => classNameList.filter((className) => className).join(' ');
 
 export default cn;
