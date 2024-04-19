c5c20fcbdd1325fecef1847aba22e017c45d9227
Taejin Kim <rlaxowls1316@naver.com>
2024-04-20 00:07:44 +0900

    Feat: 테이블 컴포넌트 수정

--- src/components/common/Table/Table.tsx
+++ src/components/common/Table/Table.tsx
@@ -1,15 +1,5 @@
 import styles from './Table.module.scss';
 
-/*
-const datas = {
-  titles: ['신청자', '소개', '전화번호', '상태'],
-  data: [
-    ['id를 넣어주세요', '김태진', '안녕하세요', '010-3727-4228', 'pending'],
-    ['id를 넣어주세요', '김우기', '안녕하세요', '010-4987-4228', 'pending'],
-  ],
-};
-*/
-
 type Datas = {
   titles: string[];
   data: string[][];
