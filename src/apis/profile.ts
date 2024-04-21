46e1f755f784fd4c26c93960dbf59701f831be37
Taejin Kim <rlaxowls1316@naver.com>
2024-04-21 14:22:12 +0900

    Feat: setUserProfile api 함수 추가

--- src/apis/profile.ts
+++ src/apis/profile.ts
@@ -12,4 +12,22 @@ export const getUserProfile = async (userId: string): Promise<UserProfileType> =
   return data.item;
 };
 
+export const putUserProfile = async (
+  userId: string,
+  body: { name: string; phone: string; address: string; bio: string },
+) => {
+  const response = await fetch(`${BASE_URL}/users/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0ODk2YzJhNi0zYzI0LTRmMjYtOWY1ZC04NGM0YjEzZGI0ZmQiLCJpYXQiOjE3MTM1ODAwNDd9.qUvw9FZS0s9sCSGniTimOVsR6bM1ej8Sh7bKPX3bpZA`,
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    throw new Error('API 오류');
+  }
+};
+
 export const error = 0;
