d86721496b3b9ee8e2df5a78c967b0ae8b350053
armd482 <armd482@naver.com>
2024-04-22 18:14:18 +0900

    Test: 테스트 코드 제거'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Header from '@/components/common/Header/Header';
-import OwnerAddNotice from '@/components/ownerAddNotice/OwnerAddNotice';
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <OwnerAddNotice />
-    </>
-  );
+  return <Header />;
 }
