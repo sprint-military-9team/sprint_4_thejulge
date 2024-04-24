4fb69148d3a72070052a124d291381d249ccb034
Taejin Kim <rlaxowls1316@naver.com>
2024-04-25 02:46:30 +0900

    Refactor: 타입오류 해결

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -2,40 +2,53 @@
 
 import { useEffect, useState } from 'react';
 import Box from './Box';
-import { items } from './mockData';
+import { items as mockItems } from './mockData';
 
 declare global {
   interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     kakao: any;
   }
 }
 
-const restaurants = [
-  {
-    address: '서가앤쿡 타임스퀘어점',
-    shop_id: 1,
-  },
-  {
-    address: '서울대학교',
-    shop_id: 2,
-  },
-  {
-    address: '서울특별시 마포구 잔다리로6길 25 재륜빌딩',
-    shop_id: 3,
-  },
-  {
-    address: '교촌치킨 신용산역점',
-    shop_id: 4,
-  },
-  {
-    address: '김정은 치킨',
-    shop_id: 4,
-  },
-];
+type Item = {
+  item: {
+    id: string;
+    hourlyPay: number;
+    startsAt: string;
+    workhour: number;
+    description: string;
+    closed: boolean;
+    shop: {
+      item: {
+        id: string;
+        name: string;
+        category: string;
+        address1: string;
+        address2: string;
+        description: string;
+        imageUrl: string;
+        originalHourlyPay: number;
+      };
+      href: string;
+    };
+  };
+  links: { rel: string; description: string; method: string; href: string }[];
+};
 
-function extractShopInfo(items) {
-  const shopInfoArray = [];
-  const shopNameList = [];
+type ShopInfo = {
+  shop_id: string;
+  shop_name: string;
+  address1: string;
+  address2: string;
+  imageUrl: string;
+  originalHourlyPay: number;
+  description: string;
+};
+
+function extractShopInfo(items: Item[]) {
+  const shopInfoArray: ShopInfo[] = [];
+  const shopNameList: string[] = [];
   items.forEach((item) => {
     const { shop } = item.item;
     if (
@@ -61,11 +74,10 @@ function extractShopInfo(items) {
       shopInfoArray.push(shopInfo);
     }
   });
-  console.log(shopInfoArray);
   return shopInfoArray;
 }
 
-const mockDatas = extractShopInfo(items);
+const mockDatas = extractShopInfo(mockItems);
 
 export default function Map() {
   const [shop, setShop] = useState({
@@ -74,6 +86,8 @@ export default function Map() {
     address1: '',
     address2: '',
     imageUrl: '',
+    description: '',
+    originalHourlyPay: '',
   });
 
   useEffect(() => {
@@ -84,38 +98,26 @@ export default function Map() {
     const container = document.getElementById('map')!;
     const onLoadKakaoMap = () => {
       window.kakao.maps.load(() => {
-        const ps = new window.kakao.maps.services.Places();
-
-        function addMarker(position, idx) {
-          // 마커 이미지 url, 스프라이트 이미지를 씁니다
-          // const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png';
-          // const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png';
+        function addMarker(position: any) {
           const imageSrc = 'http://localhost:3000/assets/gps.svg';
-          // 마커 이미지의 크기
           const imageSize = new window.kakao.maps.Size(34, 39);
-          // const imgOptions = {
-          // 스프라이트 이미지의 크기
-          // spriteSize: new window.kakao.maps.Size(40, 46),
-          // spriteOrigin: new window.kakao.maps.Point(0, idx * 16 + 10),
-          // offset: new window.kakao.maps.Point(13, 37),
-          // };
           const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
           const marker = new window.kakao.maps.Marker({
             position, // 마커의 위치
             image: markerImage,
           });
 
+          // eslint-disable-next-line @typescript-eslint/no-use-before-define
           marker.setMap(map);
 
           return marker;
         }
 
-        function displayPlaces(places: { x: number; y: number }[], shop) {
+        function displayPlaces(places: { x: number; y: number }[], shopData: ShopInfo) {
           for (let i = 0; i < places.length; i += 1) {
-            // 마커를 생성하고 지도에 표시합니다
             const placePosition = new window.kakao.maps.LatLng(places[i].y, places[i].x);
-            const marker = addMarker(placePosition, i);
-            marker.shop = shop;
+            const marker = addMarker(placePosition);
+            marker.shop = shopData;
 
             window.kakao.maps.event.addListener(marker, 'click', () => {
               setShop(marker.shop);
@@ -123,17 +125,15 @@ export default function Map() {
           }
         }
 
-        function placesSearchCB(data: string, shop) {
-          displayPlaces(data, shop);
+        function placesSearchCB(data: { x: number; y: number }[], shopData: ShopInfo) {
+          displayPlaces(data, shopData);
         }
 
         function searchPlaces() {
-          // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
           const geocoder = new window.kakao.maps.services.Geocoder();
 
           mockDatas.forEach((mockData) => {
-            // ps.keywordSearch(`${mockData.address1} ${mockData.address2}`, (data) => placesSearchCB(data, mockData));
-            geocoder.addressSearch(`${mockData.address1} ${mockData.address2}`, (data) =>
+            geocoder.addressSearch(`${mockData.address1} ${mockData.address2}`, (data: { x: number; y: number }[]) =>
               placesSearchCB(data, mockData),
             );
           });
@@ -146,7 +146,6 @@ export default function Map() {
         };
 
         const map = new window.kakao.maps.Map(container, options);
-        // map.setZoomable(false);
 
         searchPlaces();
       });
