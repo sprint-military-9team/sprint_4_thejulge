889742b305b5c45461fdd6a918683ec623a2d1c5
Taejin Kim <rlaxowls1316@naver.com>
2024-04-26 02:59:47 +0900

    UI: NoticeFeed 스타일 변경

--- src/app/map/NoticeFeed.tsx
+++ src/app/map/NoticeFeed.tsx
@@ -2,6 +2,7 @@ import getTimeDifference from '@/utils/getTimeDifference';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { PENCEL_ICON } from '@/utils/constants';
 import styles from './NoticeFeed.module.scss';
 
 type BoxProps = {
@@ -47,44 +48,45 @@ export default function NoticeFeed({ shop }: BoxProps) {
       <h2 className={styles.title}>내가 선택한 가게</h2>
 
       {Number(shop.id) < 0 ? (
-        <div>지도에서 가게를 선택해주세요</div>
+        <div className={styles.empty}>지도에서 가게를 선택해주세요</div>
       ) : (
-        <div style={{ display: 'flex', gap: '1.5rem', paddingBottom: '2rem' }}>
-          <div
-            style={{
-              width: '8rem',
-              height: '8rem',
-              position: 'relative',
-              borderRadius: '1rem',
-              overflow: 'hidden',
-              flexShrink: 0,
-            }}
-          >
+        <div className={styles.shop}>
+          <div className={styles.cover}>
             <Image fill src={shop.imageUrl} alt="af" />
           </div>
           <div>
-            <h3 style={{ marginBottom: '1rem', fontSize: '1.4rem' }}>{shop.name}</h3>
-            <p style={{ marginBottom: '1rem' }}>{shop.description}</p>
-            <p style={{ marginBottom: '1rem' }}>기존시급: {shop.originalHourlyPay}</p>
-            <p>
+            <h3 className={styles.shopName}>{shop.name}</h3>
+            <p className={styles.address}>
               {shop.address1} {shop.address2}
             </p>
+            <p className={styles.shopDescription}>{shop.description}</p>
+            <p className={styles.shopOriginalPay}>기존시급: {shop.originalHourlyPay}</p>
           </div>
         </div>
       )}
-      <h2 className={styles.title}>공고 리스트 </h2>
-      <div style={{ overflow: 'scroll', height: '50rem' }}>
-        <ul style={{ paddingTop: '2rem', display: 'flex', flexDirection: 'column', gap: '1.6rem' }}>
+      <h2 className={`${styles.title} ${styles.noticeTitle}`}>
+        <Image width={20} height={13} src={PENCEL_ICON} alt="pencil" />
+        <p className={styles.shopListTitle}>공고 리스트</p>
+      </h2>
+      <div className={styles.shopListContainer}>
+        <ul className={styles.shopList}>
           {state.map((item) => (
-            <li key={item.item.id} style={{ borderBottom: '1px solid #aaa', paddingBottom: '1rem' }}>
-              <div>
-                <h3>{item.item.description}</h3>
-                <p>시급: {item.item.hourlyPay}</p>
-                <p>{item.item.description}</p>
-                {/* 지난 공고는 띄워주지를 말자 */}
-                <p>{getTimeDifference(item.item.startsAt, item.item.workhour)}</p>
-                <p>{item.item.closed ? '마감' : ''}</p>
-                <Link href="/">공고 보러가기</Link>
+            <li key={item.item.id} className={styles.shopListItem}>
+              <div style={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'space-between' }}>
+                <div>
+                  <h3 className={styles.itemDes}>{item.item.description}</h3>
+                  <p className={styles.itemPay}>시급: {item.item.hourlyPay}</p>
+                  {/* 지난 공고는 띄워주지를 말자 */}
+                  <p className={styles.itemDay}>{getTimeDifference(item.item.startsAt, item.item.workhour)}</p>
+                  <p className={`${styles.closed} ${!item.item.closed && styles.pending}`}>
+                    {item.item.closed ? '마감' : '모집중'}
+                  </p>
+                </div>
+                <Link href="/">
+                  <button type="button" className={styles.btn}>
+                    공고 보러가기
+                  </button>
+                </Link>
               </div>
             </li>
           ))}
