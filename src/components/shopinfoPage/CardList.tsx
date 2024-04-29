c1168ee8dadc1a1d14b6439bde59c933aa0849a4
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-04-29 20:33:04 +0900

    Fix: shopinfo 페이지 공고 지난공고 로직 변경

--- src/components/shopinfoPage/CardList.tsx
+++ src/components/shopinfoPage/CardList.tsx
@@ -6,6 +6,8 @@ import styles from './CardList.module.scss';
 
 export default function CardList({ noticeList, shopData }: { noticeList: NoticeDataType[]; shopData: ShopDataType }) {
   const shopId = Cookies.get('shopId');
+  const today = new Date();
+  today.setHours(today.getHours() + 9);
   return (
     <>
       <h2 className={styles.title}>내가 등록한 공고</h2>
@@ -22,9 +24,7 @@ export default function CardList({ noticeList, shopData }: { noticeList: NoticeD
             isRaised={notice?.item.hourlyPay > shopData?.originalHourlyPay}
             location={shopData.address1}
             shopId={shopId}
-            completed={
-              notice?.item.closed ? '모집 완료' : new Date() > new Date(notice?.item.startsAt) ? '지난 공고' : ''
-            }
+            completed={notice?.item.closed ? '모집 완료' : today > new Date(notice?.item.startsAt) ? '지난 공고' : ''}
             noticeId={notice.item.id}
           />
         ))}
